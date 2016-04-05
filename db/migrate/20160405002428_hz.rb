40c7cc9e8026f5df4b246938b4b32761d76ec205
Ivan Kabashnikov <vania.kabashnikov@gmail.com>
2016-04-05 14:30:48 +0300

    migrations fixed

--- db/migrate/20160405002428_hz.rb
+++ db/migrate/20160405002428_hz.rb
@@ -4,6 +4,11 @@ class Hz < ActiveRecord::Migration
   	  t.string :category
       t.timestamps null: false
     end
+    create_table :questions do |t|
+  	  t.string :head
+  	  t.string :body
+      t.timestamps null: false
+    end
      add_reference :questions, :question_category, index: true
   end
 end
