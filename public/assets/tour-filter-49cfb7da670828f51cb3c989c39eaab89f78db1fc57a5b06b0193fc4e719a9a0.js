f46daee0638ab4c3d64b330044ebec079c127fe5
Ivan Kabashnikov <vania.kabashnikov@gmail.com>
2016-04-14 19:36:53 +0300

    sf

--- public/assets/tour-filter-49cfb7da670828f51cb3c989c39eaab89f78db1fc57a5b06b0193fc4e719a9a0.js
+++ public/assets/tour-filter-49cfb7da670828f51cb3c989c39eaab89f78db1fc57a5b06b0193fc4e719a9a0.js
@@ -1,3 +1,58 @@
+ function showHotels( hotels, requestId ){
+            var req = requestId == undefined ? "" : "?requestId=" + requestId;
+              for(var i=0; i<hotels.length; i++){
+                var stars = "";
+                for(var j = 1; j <= hotels[i]['stars_count']; j++){
+                  stars += '<img src="/assets/star-4226a930be3bd3b1e768b47996cd08066bfde1629c5197d05b7e2cbf354d7703.png" width="12" height="12" alt="">'
+                }
+                // var hotel = hotels[i];
+              var hotel = '<div class="col-xs-12 col-sm-6 col-md-6 col-large-4 \
+              tour-column"> \
+      <div class="tour-block tour-block-new"> \
+            <div class="tour-header row"> \
+            <h2 class="col-md-7"> \
+            <a href="/hotel/' + hotels[i]['id'] + req + '"> \
+            ' + hotels[i]['name'] + ' </a> \
+            </h2> \
+              <div class="tour-rating col-md-5"> \
+                <div class="tour-rating--star">' + stars +'</div> \
+                <span class="tour-rating--num">~ \
+                ' + hotels[i]['rating'].toFixed(1) + '</span> \
+              </div> \
+            </div> \
+            <div class="tour-desc">' + hotels[i]['position_info'] + ' \
+            </div> \
+        <div class="thumb"> \
+          <a href="/hotel/' + hotels[i]['id'] + req + '" class="thumb-link"> \
+          <img src="' + hotels[i]['image_url'] + '"></a> \
+          <a href="#" class="like like-left"></a> \
+        </div> \
+        <div class="tour-footer"> \
+          <div class="col-sm-5"> \
+            <div class="tour-eat"> \
+              <span>' + meal(hotels[i]['meal']) + '</span> \
+          ' + hotels[i]['depart_date'] + ' на ' + hotels[i]['nights'] + ' ночей \
+            </div> \
+          </div> \
+          <div class="col-sm-7"> \
+            <div class="text-right"> \
+            <a href="/hotel/' + hotels[i]['id'] + req + ' \
+            #comments" class="tour-commentlink"> \
+            Отзывы (' + hotels[i]['reviews_count'] + ')</a> \
+            </div> \
+            <div class="text-right"> \
+              <a href="/hotel/' + hotels[i]['id'] + req + '" \
+               class="tour-price"> \
+                ' + hotels[i]['price'] + '<i class="fa fa-rub"></i> \
+              </a> \
+            </div> \
+          </div> \
+        </div> \
+      </div> \
+    </div>' 
+                $('#hotels').append(hotel);
+              }
+            }
 $(document).ready(function(){
 	var filterPrice = {
 		$priceMin : $('.tourFilterPriceMin'),
@@ -120,8 +175,23 @@ $(document).ready(function(){
 		});
 		console.clear();
 		console.log(filterValue);
-		$.get( "/check", filterValue );
-	}
+		var requestId = $('#requestId').val();
+		var params = {
+  		"filter": filterValue,
+  		"requestId": requestId
+		};
+		$.ajax({
+		    url: "/filter",
+		    data: params,
+		    success: function(data){
+		      $('#hotels').html('');
+		      showHotels(data["hotels"], requestId);
+		      $('#total').html('');
+		      $('#total').html('Найдено '+ data["total"] + ' отелей');
+		      $('.more').html('');
+		    }
+		    })
+			}
 	$('.tour-filter--hide').click(function(){
 		$('.tour-filter--container').stop(true).slideUp(200);
 		$('.tour-filter--show').delay(150).stop(true).animate({'opacity' : 1},150);
