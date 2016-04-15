8f3db7bf03d00ab817e2145fa58f89f53c5120bd
Ivan Kabashnikov <vania.kabashnikov@gmail.com>
2016-04-15 10:57:33 +0300

    gh

--- lib/sletat.rb
+++ lib/sletat.rb
@@ -109,6 +109,7 @@ module Sletat
    # s_departFrom = Date.parse(params['s_departFrom'])
    # s_departTo = s_departFrom + 45.days
     meals = params[:meals].to_i > 0 ? params[:meals] : nil
+    stars = [402,403,404]
    # hotel = Hotel.find(params[:hotel_id]) if params[:hotel_id]
    # hotels = hotel.sletat_id if hotel
    # countryId = params[:countryId] || (hotel.resort.country.sletat_id if hotel)
@@ -116,7 +117,7 @@ module Sletat
                    s_adults: s_adults, s_kids: s_kids, s_nightsMin: s_nightsMin, s_nightsMax: s_nightsMax,
                    s_priceMin: priceMin, s_priceMax: priceMax, s_departFrom: s_departFrom.strftime("%d/%m/%Y"),
                    s_departTo: s_departTo.strftime("%d/%m/%Y"), s_hotelIsNotInStop: true, s_hasTickets: true,
-                   includeDescriptions: 1, updateResult: 0, hotels: hotels, meals: meals}
+                   includeDescriptions: 1, updateResult: 0, hotels: hotels, stars: stars, meals: meals}
 
     data = get_res_data 'GetTours', true, url_params
     requestId = data["requestId"]
