c1b4d91cf22b3ae7a0e5a38a121a3084d8530b9b
Ivan Kabashnikov <vania.kabashnikov@gmail.com>
2016-04-25 17:25:42 +0300

    sdf

--- db/migrate/20160425094448_add_attachment_to_reviews.rb
+++ db/migrate/20160425094448_add_attachment_to_reviews.rb
@@ -1,11 +1,4 @@
 class AddAttachmentToReviews < ActiveRecord::Migration
-   drop_table :revimgs do |t|
-      t.attachment :image
-      t.references :review, index: true, foreign_key: true
-      
-
-      t.timestamps null: false
-    end
     create_table :revimgs do |t|
       t.attachment :image
       t.references :review, index: true, foreign_key: true
