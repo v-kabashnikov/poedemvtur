e39bf2a42209b5190ccb2ed0031b4489dd02bac6
Ivan Kabashnikov <vania.kabashnikov@gmail.com>
2016-05-04 15:04:46 +0300

    sdf

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -49,7 +49,7 @@ class HomeController < ApplicationController
       end
     end
     UserMailer.buy_notification(params[:email], params[:hotel_name], params[:depart_date], params[:arrive_date]).deliver
-    UserMailer.buy_notification("jujava@mail").deliver
+    UserMailer.buy_notification("jujava@mail", params[:hotel_name], params[:depart_date], params[:arrive_date]).deliver
     redirect_to "/thanks/#{params[:id]}"
   end
 
