256b30f1893fe06ae1504aa13743fa09660cf79d
K. Kanyuka <kkan.web@gmail.com>
2016-05-07 17:00:58 +0300

    dsf

--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,11 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'heroku.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
+  :user_name => 'zakaz@poedemvtour.ru',
+  :password => 'Silena15',
+  :domain => 'poedemvtour.ru',
+  :address => 'smtp.mail.ru',
+  :port => 465,
   :authentication => :plain,
   :enable_starttls_auto => true
 }
