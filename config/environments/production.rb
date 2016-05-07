165bee7d95e4ae88f0aaaeaac8716de3f61bf2e4
K. Kanyuka <kkan.web@gmail.com>
2016-05-07 17:12:33 +0300

    mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,15 @@ Rails.application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
+  config.action_mailer.smtp_settings = {
+  :user_name => 'zakaz@poedemvtour.ru',
+  :password => 'Silena15',
+  :domain => 'poedemvtour.ru',
+  :address => 'smtp.mail.ru',
+  :port => 465,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+  }
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
