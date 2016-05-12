3ea24f07f21ba197d010e0475ef19fe26998c8e4
Ivan Kabashnikov <vania.kabashnikov@gmail.com>
2016-05-12 20:17:16 +0300

    fixed https aws

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,5 +1,6 @@
 Paperclip::Attachment.default_options.update(
 	storage: :s3,
+	:s3_protocol => :https,
 	path: "/:class/:attachment/:id_partition/:style/:filename",
 	s3_host_name: "s3-eu-central-1.amazonaws.com",
 	url: ":s3_domain_url",
